83958263f7756685e0460524e6edda1be8d9db5b
Dilshan Pathirana <s19121@sci.pdn.ac.lk>
2026-01-29 13:39:38 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const repo = 'Sama-frontend';
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
